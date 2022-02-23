b49c6a2e4647021a1385df25836269bd8347605a
ayachi chaker <chaker.ayachi@esprit.tn>
2022-02-23 21:01:50 +0100

    adding start

--- src/Controllers/LoadingFXMLController.java
+++ src/Controllers/LoadingFXMLController.java
@@ -5,13 +5,25 @@
  */
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.RotateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
 
 /**
  * FXML Controller class
@@ -39,6 +51,8 @@ public class LoadingFXMLController implements Initializable {
         setRotate(c2, true, 180, 18);
         setRotate(c3, true, 145, 24);
         setRotate(c4, true, 260, 13);
+        
+        
     }  
     
         int rotate = 0;
@@ -51,6 +65,26 @@ public class LoadingFXMLController implements Initializable {
         rotateTransition.setCycleCount(18);
         rotateTransition.play();
         
+        
+        
     }
+
+    @FXML
+    private void ChangerScene(ActionEvent event) {
+        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/LoginFXML.fxml"));
+            Parent root = loader.load();
+            c1.getScene().setRoot(root);
+        } catch (IOException ex) {
+            Logger.getLogger(LoadingFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+   
+
+
+   
+
     
 }
